7aecf8fe0fc4f8195ddd0dac805acfd4c548d9c9
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-07-09 03:10:01 +0530

    Update axiosClient.js

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'http://127.0.0.1:5000/api/v1/'
 const getToken = () => localStorage.getItem('token')
 
 const axiosClient = axios.create({
-  baseURL: baseUrl,
+  baseURL: process.env.REACT_APP_API_URL,
   paramsSerializer: params => queryString.stringify({ params })
 })
 
