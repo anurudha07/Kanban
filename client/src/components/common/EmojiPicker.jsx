771832b024ae052945030d6fd7e6455853595f9a
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-07-09 17:36:47 +0530

    Update EmojiPicker.jsx

--- client/src/components/common/EmojiPicker.jsx
+++ client/src/components/common/EmojiPicker.jsx
@@ -1,10 +1,11 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material'
 import React, { useState, useEffect } from 'react'
 import { Picker } from 'emoji-mart'
-
 import 'emoji-mart/css/emoji-mart.css'
 
 const EmojiPicker = props => {
+  const theme = useTheme()
+  const isXs = useMediaQuery(theme.breakpoints.down('sm'))
   const [selectedEmoji, setSelectedEmoji] = useState()
   const [isShowPicker, setIsShowPicker] = useState(false)
 
@@ -14,8 +15,7 @@ const EmojiPicker = props => {
 
   const selectEmoji = (e) => {
     const sym = e.unified.split('-')
-    let codesArray = []
-    sym.forEach(el => codesArray.push('0x' + el))
+    const codesArray = sym.map(el => '0x' + el)
     const emoji = String.fromCodePoint(...codesArray)
     setIsShowPicker(false)
     props.onChange(emoji)
@@ -36,8 +36,12 @@ const EmojiPicker = props => {
       <Box sx={{
         display: isShowPicker ? 'block' : 'none',
         position: 'absolute',
-        top: '100%',
-        zIndex: '9999'
+        top: isXs ? 'auto' : '100%',
+        bottom: isXs ? 0 : 'auto',
+        left: isXs ? 0 : 'auto',
+        width: isXs ? '100vw' : 'auto',
+        height: isXs ? '50vh' : 'auto',
+        zIndex: 9999
       }}>
         <Picker theme='dark' onSelect={selectEmoji} showPreview={false} />
       </Box>
