f514a54bb98685178199ca43b29b4bdc1f4dfc13
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-07-10 20:36:22 +0530

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,7 +6,7 @@ import favouriteReducer from './features/favouriteSlice'
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    board: boardReducer,          // matches slice.name = 'board'
-    favourites: favouriteReducer, // matches slice.name = 'favourites'
+    board: boardReducer,          
+    favourites: favouriteReducer, 
   }
 })
