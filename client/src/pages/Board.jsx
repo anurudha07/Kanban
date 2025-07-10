a0fda9457b93d060ff098124463ced3ba2700789
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-07-10 20:25:02 +0530

    Update Board.jsx

--- client/src/pages/Board.jsx
+++ client/src/pages/Board.jsx
@@ -1,199 +1,166 @@
+// src/pages/Board.jsx
+import React, { useEffect, useState, useRef } from 'react'
+import { Box, IconButton, TextField, Typography, useTheme, useMediaQuery } from '@mui/material'
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined'
 import StarOutlinedIcon from '@mui/icons-material/StarOutlined'
-import { Box, IconButton, TextField } from '@mui/material'
-import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate, useParams } from 'react-router-dom'
 import boardApi from '../api/boardApi'
 import EmojiPicker from '../components/common/EmojiPicker'
 import Kanban from '../components/common/Kanban'
-import { setBoards } from '../redux/features/boardSlice'
-import { setFavouriteList } from '../redux/features/favouriteSlice'
-
-let timer
-const timeout = 500
+import { setBoards, updateBoard, removeBoard } from '../redux/features/boardSlice'
+import { setFavouriteList, updateFavourite, removeFavourite } from '../redux/features/favouriteSlice'
 
 const Board = () => {
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+  const [sections, setSections] = useState([])
+  const [fav, setFav] = useState(false)
+  const [icon, setIcon] = useState('')
+  const [justSaved, setJustSaved] = useState(false)
+
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const { boardId } = useParams()
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [sections, setSections] = useState([])
-  const [isFavourite, setIsFavourite] = useState(false)
-  const [icon, setIcon] = useState('')
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  const favourites = useSelector(s => s.favourites.value)
 
-  const boards = useSelector((state) => state.board.value)
-  const favouriteList = useSelector((state) => state.favourites.value)
+  const titleTimer = useRef()
+  const iconTimer  = useRef()
 
+  // Load board data on mount or when boardId changes
   useEffect(() => {
-    const getBoard = async () => {
-      try {
-        const res = await boardApi.getOne(boardId)
-        setTitle(res.title)
-        setDescription(res.description)
-        setSections(res.sections)
-        setIsFavourite(res.favourite)
-        setIcon(res.icon)
-      } catch (err) {
-        alert(err)
-      }
-    }
-    getBoard()
+    boardApi.getOne(boardId)
+      .then(r => {
+        setTitle(r.title)
+        setDesc(r.description || '')
+        setSections(r.sections || [])
+        setFav(r.favourite)
+        setIcon(r.icon)
+      })
+      .catch(console.error)
   }, [boardId])
 
-  const onIconChange = async (newIcon) => {
-    let temp = [...boards]
-    const index = temp.findIndex(e => e.id === boardId)
-    temp[index] = { ...temp[index], icon: newIcon }
+  // Save changes then re-fetch full boards list
+  const save = changes =>
+    boardApi.update(boardId, changes)
+      .then(updated => {
+        dispatch(updateBoard(updated))
+        dispatch(updateFavourite(updated))
+        return boardApi.getAll()
+      })
+      .then(all => {
+        dispatch(setBoards(all))
+        setJustSaved(true)
+        setTimeout(() => setJustSaved(false), 1500)
+      })
+      .catch(console.error)
 
-    if (isFavourite) {
-      let tempFavourite = [...favouriteList]
-      const favouriteIndex = tempFavourite.findIndex(e => e.id === boardId)
-      tempFavourite[favouriteIndex] = { ...tempFavourite[favouriteIndex], icon: newIcon }
-      dispatch(setFavouriteList(tempFavourite))
-    }
+  // Title change handler
+  const onTitleChange = e => {
+    const v = e.target.value
+    setTitle(v)
+    dispatch(updateBoard({ id: boardId, title: v }))
 
+    clearTimeout(titleTimer.current)
+    titleTimer.current = setTimeout(() => save({ title: v }), 500)
+  }
+
+  // Emoji change handler
+  const onIconChange = newIcon => {
     setIcon(newIcon)
-    dispatch(setBoards(temp))
-    try {
-      await boardApi.update(boardId, { icon: newIcon })
-    } catch (err) {
-      alert(err)
-    }
+    dispatch(updateBoard({ id: boardId, icon: newIcon }))
+    dispatch(updateFavourite({ id: boardId, icon: newIcon }))
+
+    clearTimeout(iconTimer.current)
+    iconTimer.current = setTimeout(() => save({ icon: newIcon }), 500)
   }
 
-  const updateTitle = async (e) => {
-    clearTimeout(timer)
-    const newTitle = e.target.value
-    setTitle(newTitle)
-
-    let temp = [...boards]
-    const index = temp.findIndex(e => e.id === boardId)
-    temp[index] = { ...temp[index], title: newTitle }
-
-    if (isFavourite) {
-      let tempFavourite = [...favouriteList]
-      const favouriteIndex = tempFavourite.findIndex(e => e.id === boardId)
-      tempFavourite[favouriteIndex] = { ...tempFavourite[favouriteIndex], title: newTitle }
-      dispatch(setFavouriteList(tempFavourite))
-    }
-
-    dispatch(setBoards(temp))
-
-    timer = setTimeout(async () => {
-      try {
-        await boardApi.update(boardId, { title: newTitle })
-      } catch (err) {
-        alert(err)
-      }
-    }, timeout);
+  // Toggle favourite status
+  const onToggleFav = () => {
+    const nf = !fav
+    setFav(nf)
+    boardApi.update(boardId, { favourite: nf })
+      .then(updated => {
+        dispatch(updateBoard(updated))
+        if (nf) dispatch(setFavouriteList([updated, ...favourites]))
+        else   dispatch(removeFavourite(updated.id))
+      })
+      .catch(() => setFav(fav))
   }
 
-  const updateDescription = async (e) => {
-    clearTimeout(timer)
-    const newDescription = e.target.value
-    setDescription(newDescription)
-    timer = setTimeout(async () => {
-      try {
-        await boardApi.update(boardId, { description: newDescription })
-      } catch (err) {
-        alert(err)
-      }
-    }, timeout);
-  }
-
-  const addFavourite = async () => {
-    try {
-      const board = await boardApi.update(boardId, { favourite: !isFavourite })
-      let newFavouriteList = [...favouriteList]
-      if (isFavourite) {
-        newFavouriteList = newFavouriteList.filter(e => e.id !== boardId)
-      } else {
-        newFavouriteList.unshift(board)
-      }
-      dispatch(setFavouriteList(newFavouriteList))
-      setIsFavourite(!isFavourite)
-    } catch (err) {
-      alert(err)
-    }
-  }
-
-  const deleteBoard = async () => {
-    try {
-      await boardApi.delete(boardId)
-      if (isFavourite) {
-        const newFavouriteList = favouriteList.filter(e => e.id !== boardId)
-        dispatch(setFavouriteList(newFavouriteList))
-      }
-
-      const newList = boards.filter(e => e.id !== boardId)
-      if (newList.length === 0) {
-        navigate('/boards')
-      } else {
-        navigate(`/boards/${newList[0].id}`)
-      }
-      dispatch(setBoards(newList))
-    } catch (err) {
-      alert(err)
-    }
+  // Delete board
+  const onDelete = () => {
+    boardApi.delete(boardId)
+      .then(() => {
+        dispatch(removeBoard(boardId))
+        dispatch(removeFavourite(boardId))
+        navigate('/')
+      })
+      .catch(console.error)
   }
 
   return (
     <>
-      <Box sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        width: '100%'
-      }}>
-        <IconButton variant='outlined' onClick={addFavourite}>
-          {
-            isFavourite ? (
-              <StarOutlinedIcon color='warning' />
-            ) : (
-              <StarBorderOutlinedIcon />
-            )
-          }
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', px: isMobile ? 1 : 5, py: 1 }}>
+        <IconButton onClick={onToggleFav}>
+          {fav
+            ? <StarOutlinedIcon color="warning" />
+            : <StarBorderOutlinedIcon />}
         </IconButton>
-        <IconButton variant='outlined' color='error' onClick={deleteBoard}>
+        <IconButton color="error" onClick={onDelete}>
           <DeleteOutlinedIcon />
         </IconButton>
       </Box>
-      <Box sx={{ padding: '10px 50px' }}>
-        <Box>
-          {/* emoji picker */}
-          <EmojiPicker icon={icon} onChange={onIconChange} />
-          <TextField
-            value={title}
-            onChange={updateTitle}
-            placeholder='Untitled'
-            variant='outlined'
-            fullWidth
-            sx={{
-              '& .MuiOutlinedInput-input': { padding: 0 },
-              '& .MuiOutlinedInput-notchedOutline': { border: 'unset ' },
-              '& .MuiOutlinedInput-root': { fontSize: '2rem', fontWeight: '700' }
-            }}
+
+      <Box sx={{ px: isMobile ? 1 : 5, py: isMobile ? 1 : 2 }}>
+        <EmojiPicker icon={icon} onChange={onIconChange} />
+
+        <TextField
+          fullWidth
+          variant="outlined"
+          placeholder="Untitled"
+          value={title}
+          onChange={onTitleChange}
+          sx={{
+            mt: 1,
+            '& .MuiOutlinedInput-notchedOutline': { border: 'none' },
+            '& .MuiInputBase-input': {
+              fontSize: isMobile ? '1.5rem' : '2rem',
+              fontWeight: 700
+            }
+          }}
+        />
+        {justSaved && (
+          <Typography variant="caption" color="success.main" sx={{ mt: 0.5 }}>
+            Saved!
+          </Typography>
+        )}
+
+        <TextField
+          fullWidth
+          variant="outlined"
+          multiline
+          placeholder="Add a description"
+          value={desc}
+          onChange={e => setDesc(e.target.value)}
+          sx={{
+            mt: 1,
+            '& .MuiOutlinedInput-notchedOutline': { border: 'none' },
+            '& .MuiInputBase-input': {
+              fontSize: isMobile ? '0.75rem' : '0.8rem'
+            }
+          }}
+        />
+
+        <Box sx={{ mt: 2 }}>
+          <Kanban
+            data={sections}
+            boardId={boardId}
+            onSectionsChange={setSections}
           />
-          <TextField
-            value={description}
-            onChange={updateDescription}
-            placeholder='Add a description'
-            variant='outlined'
-            multiline
-            fullWidth
-            sx={{
-              '& .MuiOutlinedInput-input': { padding: 0 },
-              '& .MuiOutlinedInput-notchedOutline': { border: 'unset ' },
-              '& .MuiOutlinedInput-root': { fontSize: '0.8rem' }
-            }}
-          />
-        </Box>
-        <Box>
-          {/* Kanban board */}
-          <Kanban data={sections} boardId={boardId} />
         </Box>
       </Box>
     </>
